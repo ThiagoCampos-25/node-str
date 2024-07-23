12fe3a63e25221ed1538f7e27979af7a844f7bdb
ThiagoCampos-25 <thiago.f.f.l@hotmail.com>
2024-07-23 12:47:05 -0400

    feat: Controllers

--- src/app.js
+++ src/app.js
@@ -6,32 +6,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const router = express.Router();
 
-//Carrega as Routez
-const indexRoutes = require('./routes/index');
+//Carrega as Routes
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-const create = router.post("/", (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-  const id = req.params.id;
-  res.status(201).send({
-    id: id,
-    item: req.body
-  });
-});
-
-const del = router.delete("/", (req, res, next) => {
-  res.status(200).send(req.body);
-});
-
-app.use("/", indexRoutes);
-app.use("/products", create);
-app.use("/products", put);
-app.use("/products", del);
+app.use("/", indexRoute);
+app.use("/products", productRoute);
 
 module.exports = app;
